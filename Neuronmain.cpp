3d946d2ce85167e7fcd2b3734bb265857657d952
stimpfli <stimpfli@SVBBCF20170044.intranet.epfl.ch>
2017-10-02 14:35:24 +0200

    fixed main

--- Neuronmain.cpp
+++ Neuronmain.cpp
@@ -6,8 +6,8 @@ int main()
 {
 	double a(0);
 	double b(0);
-	cout<<"Entrez la borne inf du temps de simulation : a = "<<;
-	cin>>a>>endl;
-	cout<<"Entrez la borne sup du temps de simulation : b = "<<;
-	cin>>b>>endl;
+	cout<<"Entrez la borne inf du temps de simulation : a = "<<endl;
+	cin>>a;
+	cout<<"Entrez la borne sup du temps de simulation : b = "<<endl;
+	cin>>b;
 }
